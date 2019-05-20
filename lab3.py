75db7fcea3d347cf1ae74b4800ad94fd6aab326f
Karol Wojtulewicz <Wojak27@users.noreply.github.com>
2019-05-20 10:47:05 +0200

    Pseudocode lab3

--- lab3.py
+++ lab3.py
@@ -17,9 +17,30 @@ if __name__ == "__main__":
     # divide by the smoothing factor and kernelize with 
     # exp(-abs(u)**2) (abs is not needed because of **2)
     def kernelizeDistance(line):
-      print(line[3], line[4])
-      return exp(-(haversine(float(line[4]), float(line[3]), b, a))**2/h_distance)
+      return (int(line[0]), exp(-(haversine(float(line[4]), float(line[3]), b, a)/h_distance)**2))
+  
+    def kernelizeTime(line):
+      # hours between times
+      print("lol")
+      
 
+    def kernelizeDate(line):
+      # splitting the date for the datetime
+      splittedLine = line.split(";")
+      splittedDate = splittedLine[1].split("-")
+      # filter months and days that have 0 in front like 08 to 8
+      month = splittedDate[1]
+      day = splittedDate[2]
+      if month[0] == 0:
+          month = month[1]
+      if day[0] == 0:
+          day = day[1]
+      currentDate = datetime(int(splittedDate[0]),int(splittedDate[1]),int(splittedDate[2]))
+      difference = myDate - currentDate
+      # print the day difference
+      return (splittedLine[0], exp(-(difference.days/h_date)**2))
+    
+      
     sc = SparkContext(appName="lab_kernel") 
 
     def haversine(lon1, lat1, lon2, lat2):
@@ -32,23 +53,64 @@ if __name__ == "__main__":
         dlat = lat2 -lat1
         a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2 
         c = 2 * asin(sqrt(a))
-        km = 6367 * c 
-        print(km)
+        km = 6367 * c
         return km
 
 
 
-    h_distance = 10000# Up to you
-    h_date  =  1#  Up  to  you
+    h_distance = 100# Up to you
+    h_date  =  100#  Up  to  you
     h_time = 1# Up to you
     a = 60.4274 # Up to you
     b = 13.826 # Up to you
-    date = "2013-07-04" # Up to you
-
+    myDate = datetime(2008, 8, 18)  # Up to you
+    
     stations = sc.textFile("stations.csv").map(lambda x: x.split(";"))
-    temps = sc.textFile("temps50k.csv")
-    print("loading files succeeded")
+    temps = sc.textFile("temperature-readings.csv")
 
-    distance = stations.map(kernelizeDistance)
+    distanceKernel = stations.map(kernelizeDistance)
+    
+    daysKernel = temps.map(kernelizeDate)
 
-    print(distance.take(10))
+    # Broadcast the values for distance and date
+    distanceKernel = distanceKernel.collectAsMap()
+    #bcDistance = sc.broadcast(distance)
+
+    daysKernel = daysKernel.collectAsMap()
+
+#bcDays = sc.broadcast(days)
+
+#temps = temps.map(lambda x: (x, bcDays.value[x[0]], bcDistance.value[x[0]]))
+
+#   time = 0
+#   arrayMulti = []
+#   while currTime < 24:
+#       arrayMulti.append(temps.map(calcTotMultiKernel))
+#       time += 2
+
+
+# def calcTotMultiKernel(line):
+#   currTemp = line[-3]
+#   currKernDate = line[-2]
+#   currKernDist = line[-1]
+#   currKernTime = exp(-(int((line[-4].split(:)[0])-time)/h_time) ** 2)
+#   return currTemp * currKernDate * currKernDist * currKernTime
+
+#   time = 0
+#   arrayAdd = []
+#   while currTime < 24:
+#       arrayAdd.append(temps.map(calcTotAddKernel))
+#       time += 2
+
+
+# def calcTotAddKernel(line):
+#   currTemp = line[-3]
+#   currKernDate = line[-2]
+#   currKernDist = line[-1]
+#   currKernTime = exp(-(int((line[-4].split(:)[0])-time)/h_time) ** 2)
+#   return currTemp * (currKernDate + currKernDist + currKernTime)
+
+
+#print(distance.take(10))
+print(temps.take(10))
+
